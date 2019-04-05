df33fc6e4c4f2a56385809989eabccd1d969d7a3
Lucien George <luciengeorge95@gmail.com>
2019-04-05 14:44:47 +0100

    added trix library and finished trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,47 @@
 class TripsController < ApplicationController
+  before_action :find_trip, only: %i[show edit update destroy]
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @trip.update(trip_params)
+    if @trip.save
+      redirect_to trip_path(@trip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:title, :description, :destination, :price)
+  end
 end
