4954b8850cbb6559caf9eaae1f22357c6951ed75
Lucien George <luciengeorge95@gmail.com>
2019-04-09 11:55:32 +0100

    pundit done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,14 @@
 class OrdersController < ApplicationController
   def show
     @order = current_user.orders.where(state: 'paid').find(params[:id])
+    authorize @order
   end
 
   def create
     trip = Trip.find(params[:trip_id])
-    order = Order.create(trip_sku: trip.sku, amount: trip.price, state:'pending', user: current_user)
+    order = Order.new(trip_sku: trip.sku, amount: trip.price, state:'pending', user: current_user)
+    authorize order
+    order.save
     redirect_to new_order_payment_path(order)
   end
 end
---
+++ app/policies/order_policy.rb
@@ -0,0 +1,15 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    user
+  end
+
+  def create?
+    user
+  end
+end
